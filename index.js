0a010e706e8fef160cea8077ce7a911d18e08b89
STB a.k.a. Satomaru <Satomaru@users.noreply.github.com>
2020-10-24 16:12:24 +0900

    expressの起動処理を追加。

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
+const multipart = require('connect-multiparty');
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+
+class Server {
+
+  init(config) {
+    this.app = express();
+
+    this.middleware = webpackMiddleware(webpack(config), {
+      publicPath: config.output.publicPath,
+      serverSideRender: false,
+      watchOptions: {
+        ignored: /.*/
+      }
+    });
+
+    app.use(this.middleware);
+    app.use(multipart());
+    app.use(express.static('public'));
+  }
+
+  use(path, object) {
+    this.app.use(path, object);
+  }
+
+  listen() {
+    const port = process.env['REACT_APP_PORT'];
+    app.listen(port, () => console.log(`Launching... http://localhost:${port}\n`));
+
+    try {
+      pjs.register(this.app, this.middleware);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
 const playJsExpress = {
+  Server: Server
 }
 
 module.exports = playJsExpress;
